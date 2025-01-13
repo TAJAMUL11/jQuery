8a444aaa678a4e9635f5f049830bf31ed75212ff
Tajamul Wani <thetj4054@gmail.com>
2025-01-13 15:12:03 +0530

    First commit

---
+++ Introduction/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    $('.h1').click((e) => {
+        console.log("Header Clicked")
+     //    $('.h1').hide(); // hides all
+     //    $(this).hide();
+         
+     });
+     
+
+})
+
+
+
+
