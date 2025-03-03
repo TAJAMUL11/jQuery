5bb5d3e8f06993f6cb3785611951fa4dda5eca30
Tajamul Wani <thetj4054@gmail.com>
2025-03-03 10:58:55 +0530

    Slider folder added

--- Introduction/script.js
+++ Introduction/script.js
@@ -1,14 +1,22 @@
-$(document).ready(function () {
-    $('.h1').click((e) => {
-        console.log("Header Clicked")
-     //    $('.h1').hide(); // hides all
-     //    $(this).hide();
+const accordionHeaders = document.querySelectorAll('.accordion-header');
+
+// accordionHeaders.forEach(header => {
+//     header.addEventListener('click', () => {
+//         header.classList.toggle('active');
+//         const content = header.nextElementSibling;
+//         content.classList.toggle('show');
+//     });
+// });
+
+// $(document).ready(function () {
+//     $('.h1').click((e) => {
+//         console.log("Header Clicked")
+//      //    $('.h1').hide(); // hides all
+//      //    $(this).hide();
          
-     });
-     
+//      });     
 
-})
-
+// })
 
 
 
---
+++ slider/index.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+  $(".slider").slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 59000,
+    arrows: false,
+  });
+});
